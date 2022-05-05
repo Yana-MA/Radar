63ab287d56ad81dcb807e2c7d3aa48ba680690ec
Yana-MA <19862702+Yana-MA@users.noreply.github.com>
2022-05-05 20:37:59 +0300

    Initial Commit

---
+++ TestTask_Radar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestTask_Radar
+//
+//  Created by Yana on 5/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestTask_Radar/TestTask_RadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask_RadarApp.swift
+//  TestTask_Radar
+//
+//  Created by Yana on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TestTask_RadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
